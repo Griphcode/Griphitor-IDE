fdc7b77723424e5a97c5b6d0416c7f854f54c364
oxmc <67136658+oxmc@users.noreply.github.com>
2021-11-13 00:23:52 -0800

    Add files via upload

--- src/renderer/ide/assets/internal/js/app.js
+++ src/renderer/ide/assets/internal/js/app.js
@@ -1,7 +1,7 @@
 /*NodeJS Modules*/
-//var fs = require('fs');
-//var path = require('path');
-//const { ipcRenderer, ipcMain } = require('electron')
+var compressor = require('node-minify');
+var path = require('path');
+const { globalShortcut, BrowserWindow } = require('electron')
 
 /*Get buttons*/
 var RunCodeBtn = document.getElementById('RunCodeBtn');
@@ -9,6 +9,11 @@ var ExprtBtn = document.getElementById('ExprtBtn');
 var ExprtBtn1 = document.getElementById('ExprtBtn1');
 var CloseErrorBtn = document.getElementById('CloseErrorBtn');
 var OpenBtn = document.getElementById('OpenBtn');
+
+var finddialog = document.getElementById('finddialog');
+var find = document.getElementById('findbtn');
+var findtext = document.getElementById('findtext');
+var cancel = document.getElementById('cancelfindbtn');
 
 /*Get notification boxes*/
 var NotifBox = document.getElementById('Notif');
@@ -23,7 +28,8 @@ var fileOpenInput = document.getElementById('file-open-input');
 
 /*DEBUG*/
 function logval() {
-  console.log(cm.getValue());
+  console.log(rawusercode);
+  console.log(libpath);
 }
 
 var editorWrapper = document.getElementsByClassName('editor')[0];
@@ -43,23 +49,30 @@ window.addEventListener('DOMContentLoaded', function(e) {
 
 /* Buttons */
 RunCodeBtn.addEventListener("click", () => {
-  if (cm.getValue() == '') {
+  var rawusercode = cm.getValue();
+  var libpath = path.join(__dirname, "/assets/lib");
+  if (rawusercode == '') {
+    Notif.classList.add("danger");
     play('assets/internal/sound/error.mp3');
     console.log("Input empty.");
     NotifTitle.innerHTML = 'Error:';
     NotifMessage.innerHTML = 'No code to execute, please fill in the editor.';
     Notif.style.display='block';
   } else {
-    logval();
+    //logval();
     try {
       var popupWinWidth = "400";
       var popupWinHeight = "400";
       var left = (screen.width / 2) - (popupWinWidth / 2);
 	    var top = (screen.height / 2) - (popupWinHeight / 2);
       var win = window.open("", "Your Code", `resizable=yes, width=${popupWinWidth}, height=${popupWinHeight}, top=${top}, left=${left}`);
-      win.document.body.innerHTML = cm.getValue();
+      var usercode = replaceAll(rawusercode, "{!GriphLib}", libpath)
+      //var usercode = rawusercode.replace('{!GriphLib}'g, libpath);
+      //console.log(usercode);
+      win.document.body.innerHTML = usercode
       play('assets/internal/sound/success.mp3');
     } catch (e) {
+      Notif.classList.add("danger");
       console.log(e);
       play('assets/internal/sound/error.mp3');
       NotifTitle.innerHTML = 'Error:';
@@ -72,10 +85,15 @@ RunCodeBtn.addEventListener("click", () => {
 
 CloseErrorBtn.addEventListener("click", () => {
   Notif.style.display='none';
+  Notif.classList.remove("danger");
+  Notif.classList.remove("info");
+  Notif.classList.aremove("warning");
+  Notif.classList.remove("success");
 });
 
 ExprtBtn.addEventListener("click", () => {
   if (cm.getValue() == '') {
+    Notif.classList.add("danger");
     play('assets/internal/sound/error.mp3');
     console.log("All inputs are empty.");
     NotifTitle.innerHTML = 'Error:';
@@ -83,6 +101,7 @@ ExprtBtn.addEventListener("click", () => {
     NotifCheckbox.style.display='none';
     Notif.style.display='block';
   } else {
+    Notif.classList.add("info");
     NotifBoxTitle.innerHTML = 'What do you want to export this as?';
     NotifMessage.innerHTML = '';
     NotifCheckbox.style.display='block';
@@ -94,7 +113,12 @@ ExprtBtn.addEventListener("click", () => {
         NotifMessage.innerHTML = 'Exporting file...';
         NotifCheckbox.style.display='none';
         try {
-          GenFile(cm.getValue(), "html");
+          var rawusercode = cm.getValue();
+          var usercode = rawusercode.replace(`{!GriphLib}/css/fa-4.7.0-min.css`, `https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css`);
+          usercode = usercode.replace(`{!GriphLib}/css/fa-5.7.0-min.css`, `https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.0/css/font-awesome.min.css`);
+          usercode = usercode.replace(`{!GriphLib}/css/tailwindcss-2.2.16-min.css`, `https://unpkg.com/tailwindcss@2.2.16/dist/tailwind.min.css`);
+          GenFile(usercode, "html");
+          Notif.classList.add("success");
           play('assets/internal/sound/success.mp3');
           NotifTitle.innerHTML = 'Export Complete!';
           NotifMessage.innerHTML = 'Exporting Completed!';
@@ -102,6 +126,7 @@ ExprtBtn.addEventListener("click", () => {
           Notif.style.display='block';
         } catch (e) {
           console.log(e);
+          Notif.classList.add("danger");
           play('assets/internal/sound/error.mp3');
           NotifTitle.innerHTML = 'Error:';
           NotifMessage.innerHTML = `An error has accured, here is the error: \n${e}`;
@@ -109,12 +134,14 @@ ExprtBtn.addEventListener("click", () => {
           Notif.style.display='block';
         };
       } else if (NotifCheckbox2.checked == true) {
+        Notif.classList.add("info");
         console.log("Exporting as zip");
         NotifTitle.innerHTML = 'Exporting';
         NotifMessage.innerHTML = 'Exporting files...';
         NotifCheckbox.style.display='none';
         try {
           MakeZip(cm.getValue());
+          Notif.classList.add("success");
           play('assets/internal/sound/success.mp3');
           NotifTitle.innerHTML = 'Export Complete!';
           NotifMessage.innerHTML = 'Exporting Completed!';
@@ -122,6 +149,7 @@ ExprtBtn.addEventListener("click", () => {
           Notif.style.display='block';
         } catch (e) {
           console.log(e);
+          Notif.classList.add("danger");
           play('assets/internal/sound/error.mp3');
           NotifTitle.innerHTML = 'Error:';
           NotifMessage.innerHTML = `An error has accured, here is the error: \n${e}`;
@@ -131,7 +159,7 @@ ExprtBtn.addEventListener("click", () => {
       };
     });
   };
-})
+});
 
 OpenBtn.addEventListener("click", () => {
   if (fileOpenInput) {
@@ -142,10 +170,48 @@ OpenBtn.addEventListener("click", () => {
     var file = fileOpenInput.files[0];
     var reader = new FileReader();
     console.log(file.type);
-
     reader.onload = function(e) {
         cm.setValue(reader.result);
     }
     reader.readAsText(file);
   }
+});
+
+/*
+window.addEventListener("keydown",function (e) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+        e.preventDefault();
+        finddialog.style.display = "block";
+        if (true) {
+
+        }
+        let win = global.PageView
+        var options = {
+            forward: true,
+            findNext: false,
+            matchCase: false,
+            wordStart: false,
+            medialCapitalAsWordStart: false
+        }
+        findbtn.addEventListener('click', () => {
+          var text = document.getElementById('findtext').value;
+          console.log(text);
+          if (text) {
+            const requestId = win.webContents.findInPage(text, options);
+            console.log(requestId);
+          } else {
+            console.log('Enter Text to find');
+          }
+          win.webContents.on('found-in-page', (event, result) => {
+              console.log(result.requestId);
+              console.log(result.activeMatchOrdinal);
+              console.log(result.matches);
+              console.log(result.selectionArea);
+          });
+        });
+        cancel.addEventListener('click', () => {
+          win.webContents.stopFindInPage('clearSelection');
+        });
+    }
 })
+*/
--- src/renderer/ide/assets/internal/js/functions.js
+++ src/renderer/ide/assets/internal/js/functions.js
@@ -1,18 +1,4 @@
 /* Functions */
-function storeData(name,value) {
-  localStorage.setItem(name, value);
-}
-
-function updateData(name,value) {
-  let data = localStorage.getItem(name);
-  localStorage.setItem(name, value);
-}
-
-function getData(name) {
-  let data = localStorage.getItem(name);
-  return data;
-}
-
 function play(url) {
   var audio = new Audio(url);
   audio.play();
@@ -24,50 +10,8 @@ function choose(arr) {
   ];
 }
 
-function openFullscreen(elem) {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-function update(text) {
-  let result_element = document.querySelector("#highlighting-content");
-  // Handle final newlines (see article)
-  if(text[text.length-1] == "\n") {
-    text += " ";
-  }
-  // Update code
-  result_element.innerHTML = text.replace(new RegExp("&", "g"), "&amp;").replace(new RegExp("<", "g"), "&lt;"); /* Global RegExp */
-  // Syntax Highlight
-  Prism.highlightElement(result_element);
-}
-
-function sync_scroll(element) {
-  /* Scroll result to scroll coords of event - sync with textarea */
-  let result_element = document.querySelector("#highlighting");
-  // Get and set x and y
-  result_element.scrollTop = element.scrollTop;
-  result_element.scrollLeft = element.scrollLeft;
-}
-
-function check_tab(element, event) {
-  let code = element.value;
-  if(event.key == "Tab") {
-    /* Tab key pressed */
-    event.preventDefault(); // stop normal
-    let before_tab = code.slice(0, element.selectionStart); // text before tab
-    let after_tab = code.slice(element.selectionEnd, element.value.length); // text after tab
-    let cursor_pos = element.selectionEnd + 1; // where cursor moves after tab - moving forward by 1 char to after tab
-    element.value = before_tab + "\t" + after_tab; // add tab char
-    // move cursor
-    element.selectionStart = cursor_pos;
-    element.selectionEnd = cursor_pos;
-    update(element.value); // Update text to include indent
-  }
+function replaceAll(string, search, replace) {
+  return string.split(search).join(replace);
 }
 
 // Function to download data to a file
@@ -78,25 +22,6 @@ function GenFile(data, filename, type) {
   var blob = new Blob([data], { type: type });
   saveAs(blob, filename);
 }
-
-function OpenFileDialog(elm) {
-  elm.click(function() {
-    var input = $(document.createElement('input'));
-    input.attr("type", "file");
-    input.trigger('click');
-    return false;
-  });
-}
-
-/*var zip = new JSZip();
-zip.file("Hello.txt", "Hello World\n");
-var img = zip.folder("images");
-img.file("smile.gif", imgData, {base64: true});
-zip.generateAsync({type:"blob"})
-.then(function(content) {
-    // see FileSaver.js
-    saveAs(content, "example.zip");
-});*/
 
 function MakeZip(data) {
   var zip = new JSZip();
