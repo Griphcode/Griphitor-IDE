52cc3e332b956afeeaa920695a348649bca04009
oxmc <67136658+oxmc@users.noreply.github.com>
2021-11-13 00:22:03 -0800

    Delete src/renderer/ide/assets/js directory

--- src/renderer/ide/assets/js/switch.js
+++
@@ -1,9 +0,0 @@
-function Viewmode() {
-  var checkBox = document.getElementById("drkmdbtn");
-  var body = document.getElementById("body");
-  if (checkBox.checked == true) {
-    body.className += 'dark';
-  } else {
-    body.classList.remove("dark");
-  };
-};
